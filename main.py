4ab52f964cc050c613798229fe66e76e63ffee66
youngchan-bae <bdp710419@daum.net>
2025-04-26 15:28:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from lightcode_api_blueprint import app as lightcode_app
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+def root():
     return {"message": "빛의 코드가 실행 중입니다."}
+
+# 기존 API들 연결
+app.mount("/", lightcode_app)
+
